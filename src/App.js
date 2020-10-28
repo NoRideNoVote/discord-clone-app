4c4849e4122cb73025350a90486a81fb4c7bb0a2
itIsForTinkeringWithApps <halabakupit@gmail.com>
2020-10-28 22:51:07 +0100

    initial commit

---
+++ src/App.js
@@ -0,0 +1,16 @@
+import React from "react";
+// import { Counter } from "./features/counter/Counter";
+import "./App.css";
+import Sidebar from "./Sidebar";
+
+function App() {
+	return (
+		<div className="app">
+			{/* --------------------------------- Sidebar --------------------------------  */}
+			<Sidebar />
+			{/* ---------------------------------- Char ----------------------------------  */}
+		</div>
+	);
+}
+
+export default App;
